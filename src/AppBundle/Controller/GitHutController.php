fbdd7b33e0d37cdc57bd93a9ee6ecb4456c4c0ec
Emmanuel Petit <e.petit18@laposte.net>
2017-08-02 09:24:42 +0200

    End of part 2 : added profile.html.twig, faking data from github

--- src/AppBundle/Controller/GitHutController.php
+++ src/AppBundle/Controller/GitHutController.php
@@ -13,6 +13,44 @@ class GitHutController extends Controller
      */
     public function githutAction(Request $request)
     {
-        return $this->render('githut/index.html.twig');
+        return $this->render('githut/index.html.twig', [
+            'avatar_url' => 'https://avatars3.githubusercontent.com/u/27859258?v=4',
+            'name' => 'Emmanuel Petit',
+            'login' => 'ManuPetit',
+            'details' => [
+                'company' => 'iiidées',
+                'location' => 'L\'Ile Rousse, Corsica, France',
+                'joined_on' => '21 April 2017'
+            ],
+            'blog' => 'http://bellerive.iiidees.com/',
+            'social_data' => [
+                "Public Repos" => 5,
+                "Followers" => 0,
+                "Following" => 0
+            ]
+        ]);
     }
+
+//    /**
+//     * @Route("/profile", name="profile")
+//     */
+//    public function profileAction(Request $request)
+//    {
+//        return $this->render('githut/profile.html.twig', [
+//            'avatar_url' => 'https://avatars3.githubusercontent.com/u/27859258?v=4',
+//            'name' => 'Emmanuel Petit',
+//            'login' => 'ManuPetit',
+//            'details' => [
+//                'company' => 'iiidées',
+//                'location' => 'L\'Ile Rousse, Corsica, France',
+//                'joined_on' => '21 April 2017'
+//            ],
+//            'blog' => 'http://bellerive.iiidees.com/',
+//            'social_data' => [
+//                "Public Repos" => 5,
+//                "Followers" => 0,
+//                "Following" => 0
+//            ]
+//        ]);
+//    }
 }
